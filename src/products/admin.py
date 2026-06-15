3ce643dd8d8be3aecde5b74a662030d06d5f9cd5
FabianRitzmann <ritzmann.fabian33@gmail.com>
2026-06-15 21:01:10 +0200

    feat: add product tags display in product.html and register Tag model in admin.py

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Product
+from .models import Category, Comment, Product, Tag
 
 
 @admin.register(Category)
@@ -13,6 +13,8 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "price", "average_rating", "rating_count", "created_at")
     list_select_related = ("category",)
+    list_filter = ("tags",)
+    filter_horizontal = ("tags",)
 
 
 @admin.register(Comment)
@@ -20,3 +22,9 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ("product", "user", "guest_name", "rating", "created_at")
     list_filter = ("rating", "created_at")
     search_fields = ("guest_name", "guest_email", "text", "user__username")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name",)
