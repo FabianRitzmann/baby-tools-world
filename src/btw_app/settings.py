5230e9b0d44341b67ff5f26ddde486d65c0e6368
FabianRitzmann <ritzmann.fabian33@gmail.com>
2026-06-22 19:57:01 +0200

    fix: add CSRF_TRUSTED_ORIGINS for server

--- src/btw_app/settings.py
+++ src/btw_app/settings.py
@@ -148,3 +148,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
